2f8fa15d9dc6f7d10be499db8f9784ea8fc923f2
akshaykumar29 <102212425+akshaykumar29@users.noreply.github.com>
2022-08-02 11:00:01 +0530

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import pandas as pd
+import redis
+import pymongo
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return {'msg': 'working'}
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
